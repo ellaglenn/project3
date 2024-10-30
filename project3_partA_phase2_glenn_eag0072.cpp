aae8f6fd10598e9ca1d2d80c6ea87285c0eba04f
Ella Glenn <eag0072.auburn.edu>
2024-10-30 17:35:31 -0500

    Commit 1: Reading inputs from a single file

--- project3_partA_phase2_glenn_eag0072.cpp
+++ project3_partA_phase2_glenn_eag0072.cpp
@@ -6,7 +6,6 @@
 #include <iomanip>
 #include <algorithm>
 #include <map>
-
 using namespace std;
 
 struct Measurement {
@@ -14,8 +13,6 @@ struct Measurement {
     int hour;
     int minute;
     int second;
-
-    // Constructor to initialize the Measurement object
     Measurement(double v, int h, int m, int s) : value(v), hour(h), minute(m), second(s) {}
 };
 
@@ -26,31 +23,19 @@ bool readFile(const string& filename, vector<Measurement>& measurements) {
         cout << "File not found. Please try again." << endl;
         return false;
     }
-
     string line;
     while (getline(infile, line)) {
         if (line.empty()) continue; // Skip empty lines
-
         stringstream ss(line);
         double value;
-        string timestamp;
-        if (!(ss >> value) || !(ss >> timestamp)) {
-            cout << "Not an input file. Illegal content/structure detected. Please try again." << endl;
-            return false;
-        }
-
-        // Split timestamp into hour, minute, second
         int hour, minute, second;
-        char dot; // For the period characters
-        stringstream ts_stream(timestamp);
-        if (!(ts_stream >> hour >> dot >> minute >> dot >> second) || dot != '.') {
+        char sep1, sep2;
+        if (!(ss >> value >> hour >> sep1 >> minute >> sep2 >> second) || sep1 != '.' || sep2 != '.') {
             cout << "Not an input file. Illegal content/structure detected. Please try again." << endl;
             return false;
         }
-
         measurements.push_back(Measurement(value, hour, minute, second));
     }
-
     return true;
 }
 
@@ -69,25 +54,24 @@ bool compareByTime(const Measurement& a, const Measurement& b) {
 // Function to calculate statistics
 void calculateStatistics(const vector<Measurement>& measurements, double& mean, double& median, double& mode) {
     if (measurements.empty()) return;
-
     double sum = 0;
     map<double, int> frequency;
-
     for (size_t i = 0; i < measurements.size(); ++i) {
         sum += measurements[i].value;
         frequency[measurements[i].value]++;
     }
-
     mean = sum / measurements.size();
-
     // Find median
     vector<double> values;
     for (size_t i = 0; i < measurements.size(); ++i) {
         values.push_back(measurements[i].value);
     }
     sort(values.begin(), values.end());
-    median = values[values.size() / 2];
-
+    if (values.size() % 2 == 0) {
+        median = (values[values.size() / 2 - 1] + values[values.size() / 2]) / 2;
+    } else {
+        median = values[values.size() / 2];
+    }
     // Find mode
     int max_freq = 0;
     for (map<double, int>::iterator entry = frequency.begin(); entry != frequency.end(); ++entry) {
@@ -104,33 +88,30 @@ int main() {
     cout << "Enter the number of files to read: ";
     cin >> numFiles;
     cin.ignore(); // Ignore the newline after the number input
-
     vector<Measurement> combinedData;
-
     for (int i = 0; i < numFiles; i++) {
         string filename;
         cout << "Enter the filename for file " << (i + 1) << ": ";
         getline(cin, filename);
-
         if (filename == "quit") {
             cout << "Input cancelled. Proceeding to calculation..." << endl;
             break;
         }
-
         vector<Measurement> measurements;
         if (readFile(filename, measurements)) {
             combinedData.insert(combinedData.end(), measurements.begin(), measurements.end());
+            cout << "The list of " << measurements.size() << " values in file " << filename << " is:" << endl;
+            for (size_t j = 0; j < measurements.size(); ++j) {
+                cout << measurements[j].value << " " << measurements[j].hour << "." << measurements[j].minute << "." << measurements[j].second << endl;
+            }
         } else {
             i--; // Retry the same file input
         }
     }
-
     // Statistics for ordered list
     sort(combinedData.begin(), combinedData.end(), compareByValue);
-    
     double mean, median, mode;
     calculateStatistics(combinedData, mean, median, mode);
-
     cout << "*** Summarized Statistics ***" << endl;
     cout << "The orderly sorted list of " << combinedData.size() << " values is:" << endl;
     for (size_t i = 0; i < combinedData.size(); ++i) {
@@ -140,7 +121,6 @@ int main() {
     cout << "The mean is " << fixed << setprecision(4) << mean << endl;
     cout << "The median is " << median << endl;
     cout << "The mode is " << mode << endl;
-
     // Statistics for chronological list
     sort(combinedData.begin(), combinedData.end(), compareByTime);
     cout << "The chronologically sorted list of " << combinedData.size() << " values is:" << endl;
@@ -149,14 +129,32 @@ int main() {
     }
     cout << endl;
     cout << "The mean is " << mean << endl; // The mean remains the same
-    cout << "The median is " << median << endl; // Adjust if needed
+    cout << "The median is " << median << endl;
     cout << "The mode is " << mode << endl;
-
     string outputFilename;
     cout << "Enter the output filename to save: ";
     cin >> outputFilename;
+    ofstream outfile(outputFilename);
+    outfile << "*** Summarized Statistics ***" << endl;
+    outfile << "The orderly sorted list of " << combinedData.size() << " values is:" << endl;
+    for (size_t i = 0; i < combinedData.size(); ++i) {
+        outfile << combinedData[i].value << " ";
+    }
+    outfile << endl;
+    outfile << "The mean is " << fixed << setprecision(4) << mean << endl;
+    outfile << "The median is " << median << endl;
+    outfile << "The mode is " << mode << endl;
+    outfile << "The chronologically sorted list of " << combinedData.size() << " values is:" << endl;
+    for (size_t i = 0; i < combinedData.size(); ++i) {
+        outfile << combinedData[i].value << " ";
+    }
+    outfile << endl;
+    outfile << "The mean is " << mean << endl;
+    outfile << "The median is " << median << endl;
+    outfile << "The mode is " << mode << endl;
+    outfile.close();
     cout << "*** File " << outputFilename << " has been written to disk ***" << endl;
     cout << "*** Goodbye. ***" << endl;
-
     return 0;
 }
+
